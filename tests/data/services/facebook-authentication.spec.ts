5223c29a8f86d82ab10bf982b8ade8be822b9837
Jessica Castro <jessica.castros@outlook.com>
2022-07-18 16:11:40 -0300

    [tests] Ensure service returns AuthenticationError when LoadFacebookApi returns undefined

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,22 +1,28 @@
+import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 
 class FacebookAuthenticationService {
   constructor (private readonly loadFacebookUser: LoadFacebookUserApiInterface) {}
 
-  async performAuth (params: FacebookAuthentication.Params): Promise<void> {
+  async performAuth (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     await this.loadFacebookUser.loadUser(params)
+
+    return new AuthenticationError('invalid credentials')
   }
 }
 
 interface LoadFacebookUserApiInterface {
-  loadUser: (params: LoadFacebookUserApi.Params) => Promise<void>
+  loadUser: (params: LoadFacebookUserApi.Params) => Promise<LoadFacebookUserApi.Result>
 }
 
 class LoadFacebookUserSpy implements LoadFacebookUserApiInterface {
   accessToken?: string
+  result = undefined
 
-  async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
+  async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
     this.accessToken = params.accessToken
+
+    return this.result
   }
 }
 
@@ -24,6 +30,8 @@ namespace LoadFacebookUserApi {
   export type Params = {
     accessToken?: string
   }
+
+  export type Result = undefined
 }
 
 describe('FacebookAuthenticationService', () => {
@@ -33,5 +41,15 @@ describe('FacebookAuthenticationService', () => {
     await sut.performAuth({ accessToken: 'any_token' })
 
     expect(loadFacebookUser.accessToken).toBe('any_token')
+  })
+
+  it('should throw an AuthenticationError if LoadFacebookUserAPI returns undefined', async () => {
+    const loadFacebookUser = new LoadFacebookUserSpy()
+    loadFacebookUser.result = undefined
+    const sut = new FacebookAuthenticationService(loadFacebookUser)
+
+    const authResult = await sut.performAuth({ accessToken: 'any_token' })
+
+    expect(authResult).toEqual(new AuthenticationError('invalid credentials'))
   })
 })
